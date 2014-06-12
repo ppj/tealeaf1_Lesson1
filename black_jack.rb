2c51e6bb1096171e4a6ed6b3fe21d860377374e8
ppj <prasanna_3003@yahoo.com>
2014-06-12 15:31:35 +1000

    added dealing to the player and basic display

--- black_jack.rb
+++ black_jack.rb
@@ -2,6 +2,59 @@
 # Procedural version of BlackJack
 require 'pry'
 
+def prompt_user(prompt_string)
+  print "#{prompt_string} >> "
+  gets.chomp
+end
+
+
+def initialize_cards(deck_count)
+  card_set = []
+  deck_count.times do
+    # Spade "\u2660", Heart "\u2665", Club "\u2663", Diamond"\u2666"
+    ["S", "H", "C", "D"].each do | suite |
+      card_set << "#{suite} a"
+      (2..10).each do |i|
+        card_set << "#{suite} #{i}"
+      end
+      ["j", "q", "k"].each do | c |
+        card_set << "#{suite} #{c}"
+      end
+    end
+  end
+  card_set
+end
+
+
+def deal_one_card(cards)
+  cards.delete_at(rand(cards.length))
+end
+
+
+def display_cards(cards)
+  cards.each { print "  --------  " }
+  puts
+
+  2.times do
+    cards.each { print " |        | " }
+    puts
+  end
+
+  cards.each { | card | print " |  %-4s  | " % card }
+  puts
+
+  2.times do
+    cards.each { print " |        | " }
+    puts
+  end
+
+  cards.each { print "  --------  " }
+  puts
+
+end
+
+
+
 # pseudo code
 # ask user to enter name
 
@@ -23,13 +76,13 @@ require 'pry'
 
   # else (i.e. player does not have blackjack)
 
-    # NOT SURE ABOUT THIS STEP ##
+    # ## NOT SURE ABOUT THIS STEP ##
     # if dealer has a blackjack (total of 21)
       # announce dealer as the winner & end game
     # end
 
-    # ask player to choose to hit or stay
-    # while player chooses to hit
+    # loop
+      # ask player to choose to hit or stay
       # deal one card to player
       # add & show player card/s, update player total
       # if player has a blackjack
@@ -37,7 +90,7 @@ require 'pry'
       # else if player total > 21
         # announce player as loser & end game
       # end
-    # end
+    # end while player chooses to hit
 
     # while dealer total < 17
       # dealer deals one card to self
@@ -63,6 +116,26 @@ require 'pry'
 
 
 
+# main
+player = prompt_user("Welcome to the BlackJack table.\nPlease enter your name")
+new_game = prompt_user("Hi #{player}, are you ready to start a new game? [y/<anything else>]").downcase[0] == 'y'? true : false
+
+while new_game
+  number_of_decks = "gibberish"
+  until [1, 2, 4, 6, 8].index(number_of_decks)
+    number_of_decks = prompt_user("Enter the number of decks you would prefer to play with [1, 2, 4, 6, 8]").to_i
+  end
+
+  card_set = initialize_cards(number_of_decks)
+  # binding.pry
+  player_cards = []
+  dealer_cards = []
+
+  2.times { player_cards << deal_one_card(card_set) }
+  display_cards player_cards
+
+  new_game = prompt_user("Hi #{player}, are you ready to start a new game? [y/n]").downcase[0] == 'y'? true : false
+end
 
 
 
